8d171064fc473a118920b343f24968463f480bb0
kevinnail <fumalicious@gmail.com>
2022-10-19 21:46:23 -0700

    some css added, redirect to profile page if no profile, otherwise go to home

--- hook/hook.js
+++ hook/hook.js
@@ -1,17 +1,39 @@
 import '../auth/user.js';
-import { getConversion } from '../fetch-utils.js';
+import { getUser, getConversion, getProfile } from '../fetch-utils.js';
+
+// DOM
+
+const continueButton = document.getElementById('continue-button');
+const conversionHeader = document.getElementById('conversion-header');
+const conversionNum1 = document.getElementById('conversion-1');
+const conversionNum2 = document.getElementById('conversion-2');
+conversionHeader.textContent = '';
+conversionNum1.textContent = '';
+conversionNum2.textContent = '';
+
+let user = getUser();
+let profile = null;
+// events
 
 window.addEventListener('load', async () => {
-    const conversionId1 = getRandomNumber(30);
-    const conversionId2 = getRandomNumber(30);
-    const conversion1 = await getConversion(conversionId1);
-    const conversion2 = await getConversion(conversionId2);
+    profile = await getProfile(user.id);
 
-    let x = conversion1 / conversion2;
-    let x2 = conversion2 / conversion1;
+    const conversionId1 = getRandomNumber(29) + 1;
+    const conversionId2 = getRandomNumber(29) + 1;
+    const conversionData1 = await getConversion(conversionId1);
+    const conversionData2 = await getConversion(conversionId2);
+    const conversion1 = conversionData1.data;
+    const conversion2 = conversionData2.data;
 
-    factorPlural = item.title_pl;
-    factorId = item.id;
+    const factorTitle = conversion1.title;
+    const factorTitle2 = conversion2.title;
+    const factorPlural = conversion1.title_pl;
+    const factorPlural2 = conversion2.title_pl;
+    let randomTitle = null;
+    let x = conversion1.weight / conversion2.weight;
+    let x2 = conversion2.weight / conversion1.weight;
+
+    // factorId = item.id;
     if (x < 0.0001) {
         x = x.toFixed(6);
         x2 = x2.toFixed(0);
@@ -28,9 +50,21 @@ window.addEventListener('load', async () => {
         x = x.toFixed(0);
         x2 = x2.toFixed(6);
     }
+    randomTitle = `${conversion1.title} at ${conversion1.weight} pounds converted into a ${conversion2.title} at ${conversion2.weight} pounds...`;
+    x = `${factorTitle} is equal to approximately ${x} ${factorPlural2}`;
+    x2 = `A ${factorTitle2} is equal to approximately ${x2} ${factorPlural}`;
 
-    x = `${refTitle} is equal to approximately ${x} ${factorPlural}`;
-    x2 = `A ${conversionSelect.value} is approximately ${x2} ${refTitle}s`;
+    conversionHeader.textContent = randomTitle;
+    conversionNum1.textContent = x;
+    conversionNum2.textContent = x2;
+});
+
+continueButton.addEventListener('click', () => {
+    if (profile.data !== null) {
+        location.replace('/');
+    } else {
+        location.replace('/profile');
+    }
 });
 
 function getRandomNumber(choices) {
